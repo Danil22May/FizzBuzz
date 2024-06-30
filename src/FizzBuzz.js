925cdb3b5e66d1b87891b5a75abb872d8ca7fee8
Danil22May <daniilmej88@gmail.com>
2024-07-01 00:31:55 +0200

    FizzBuzz done

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,20 @@
 export default class FizzBuzz {
-
-    
-
+  divisible(number) {
+    if (number % 5 === 0 && number % 3 === 0) {
+      return "FizzBuzz";
+    }
+    if (number % 3 === 0 || number.toString().includes("3")) {
+      return "Fizz";
+    }
+    if (number % 5 === 0 || number.toString().includes("5")) {
+      return "Buzz";
+    }
+  }
+  printKata() {
+    for (let i = 0; i <= 100; i++) {
+      console.log(this.divisible(i) ?? i);
+    }
+  }
 }
+const fizzBuzz = new FizzBuzz();
+fizzBuzz.printKata();
