ed5d9f013b6ac21194e6abcd237aa9ca4facab73
Danil22May <daniilmej88@gmail.com>
2024-07-01 00:36:45 +0200

    FizzBuzz fixed

--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,10 +1,36 @@
-import { describe, expect, it } from 'vitest';
-import FizzBuzz from '../src/FizzBuzz';
+import { describe, expect, it } from "vitest";
+import FizzBuzz from "../src/FizzBuzz";
 
-describe.skip('FizzBuzz', () => {
-    
-    it.skip('if number divisible by 3 return Fizz', () => {
-        
-    });
-
+describe("FizzBuzz", () => {
+  it("if number is divisible by 3 && 5 should return FizzBuz", () => {
+    //given
+    const number = 15;
+    const fizzBuzz = new FizzBuzz();
+    //when
+    const result = fizzBuzz.divisible(number);
+    //then
+    expect(result).toBeTypeOf("string");
+    expect(result).contains("FizzBuzz");
+  });
+  it("if number is divisible by 5 should return Buzz", () => {
+    //given
+    const number = 1555;
+    const fizzBuzz = new FizzBuzz();
+    //when
+    const result = fizzBuzz.divisible(number);
+    console.log(result);
+    //then
+    expect(result).toBeTypeOf("string");
+    expect(result).contains("Buzz");
+  });
+  it("if number is divisible by 3 should return Fizz", () => {
+    //given
+    const number = 13;
+    const fizzBuzz = new FizzBuzz();
+    //when
+    const result = fizzBuzz.divisible(number);
+    //then
+    expect(result).toBeTypeOf("string");
+    expect(result).contains("Fizz");
+  });
 });
